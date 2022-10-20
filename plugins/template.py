30d9cf36739de8f68fe4d3a4020fa4f2f2ae039a
GDNickYT <86964782+GDNickYT@users.noreply.github.com>
2022-10-20 18:03:21 +0300

    Create template.py

---
+++ plugins/template.py
@@ -0,0 +1,5 @@
+from main import cmd
+##commands = ["test1", "test2"]
+##if cmd in commands:
+    ##print("this is a test")
+# as you can see, making a plugin is very easy. let your imagination go on max!
